8c42d11131e43b854a17c6f2e92e1ba1000bcd39
AxoltDash <darshan@ciencias.unam.mx>
2024-09-14 17:54:49 -0600

    ♻️ Refactor code and remove commented out sections

--- ejercicio1.py
+++ ejercicio1.py
@@ -32,20 +32,20 @@ class Histograma:
         plt.show()
 
 
-datos = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5] # Creamos el conjunto de datos
-hist = Histograma(datos)
-hist.graficar_frecuencia()
-hist.graficar_frecuencia_relativa()
+# datos = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5] # Creamos el conjunto de datos
+# hist = Histograma(datos)
+# hist.graficar_frecuencia()
+# hist.graficar_frecuencia_relativa()
 
-# Transcribimos los datos de cada columna en listas
-columna_1 = [0.25, 0.87, 0.53, 0.22, 0.67, 0.14, 0.93, 0.75, 0.45, 0.56]
-columna_2 = [0.30, 1.11, 0.50, 0.90, 0.72, 1.23, 0.15, 2.10, 0.67, 1.43]
-columna_3 = [1, 3, 2, 1, 4, 2, 3, 1, 5, 2]
-columna_4 = [0.01, 0.76, 0.91, 0.03, 0.65, 0.99, 0.12, 0.84, 0.32, 0.58]
+# # Transcribimos los datos de cada columna en listas
+# columna_1 = [0.25, 0.87, 0.53, 0.22, 0.67, 0.14, 0.93, 0.75, 0.45, 0.56]
+# columna_2 = [0.30, 1.11, 0.50, 0.90, 0.72, 1.23, 0.15, 2.10, 0.67, 1.43]
+# columna_3 = [1, 3, 2, 1, 4, 2, 3, 1, 5, 2]
+# columna_4 = [0.01, 0.76, 0.91, 0.03, 0.65, 0.99, 0.12, 0.84, 0.32, 0.58]
 
-# Creamos y graficamos los histogramas para cada columna
-for i, datos in enumerate([columna_1, columna_2, columna_3, columna_4], start=1):
-    print(f"Histogramas para la Columna {i}:")
-    hist = Histograma(datos)
-    hist.graficar_frecuencia()
-    hist.graficar_frecuencia_relativa()
+# # Creamos y graficamos los histogramas para cada columna
+# for i, datos in enumerate([columna_1, columna_2, columna_3, columna_4], start=1):
+#     print(f"Histogramas para la Columna {i}:")
+#     hist = Histograma(datos)
+#     hist.graficar_frecuencia()
+#     hist.graficar_frecuencia_relativa()
